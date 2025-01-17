a1401040d36a2d698dd0bb0a6e2fb9b60b3108ba
E1617 <pinzonhelizabeth@gmail.com>
2025-01-17 17:56:43 -0500

    Funcion de sortear y validacion

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 function agregarAmigo(){
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML = "";
     let nombresAmigos = [];
     let nombre = document.getElementById("amigo");
     if(nombre.text == ""){
@@ -14,5 +16,18 @@ function listaAmigos(nombresAmigos){
     let lista = document.getElementById("listaAmigos");
     for(let i = 0; i < nombresAmigos.length; i++){
        lista.innerHTML += `<li>${nombresAmigos[i]}</li>`;
+    }
+}
+
+function sortearAmigo(){
+    let lista = document.getElementById("listaAmigos");
+    let listaAmigos = lista.querySelectorAll("li");
+    if(listaAmigos.length > 0){
+        let resultado = document.getElementById("resultado");
+        let numeroAleatorio = Math.floor(Math.random() * listaAmigos.length);
+        lista.innerHTML = "";
+        resultado.innerHTML = `<li>${listaAmigos[numeroAleatorio].innerText}</li>`;
+    }else{
+        alert("No hay amigos para sortear");
     }
 }
